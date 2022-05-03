494fc433414fb984662d30c35d8dce9183c3dcde
bcho892 <bcho892@aucklanduni.ac.nz>
2022-05-03 23:07:53 +1200

    initial commit

---
+++ Task.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Globalization;
+/// <summary>
+/// Summary description for Class1
+/// </summary>
+public enum Priority
+{
+	URGENT,
+	IMPORTANT,
+	AVERAGE,
+	TRIVIAL
+}
+
+public class Task {
+	
+		private DateTime deadline { get; set; }
+		private Priority priority { get; set; }
+		private string description { get; set; }
+		
+		public Task(Priority priority, DateTime deadline, String description)
+		{
+		this.priority = priority;
+		this.deadline = deadline;
+		this.description = description;
+		}
+
+
+}
